a80afe5e18c3ef8870d9d37d348bf0470c1da472
Debraj Kundu <debrajk27@gmail.com>
2022-03-13 16:25:34 +0530

    task completion functionality

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,38 +1,49 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-const axios = require("axios");
+import "./Home.css"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+const axios = require("axios")
 
 function Home({ setItemId }) {
-  const [item, setItem] = useState("");
-  const [items, setItems] = useState([]);
+  const [item, setItem] = useState("")
+  const [items, setItems] = useState([])
+  const [ind, setInd] = useState([])
+
 
   useEffect(() => {
     const lists = async () => {
-      let tasks = await axios.get("http://localhost:9000/todo");
-      tasks = tasks.data;
-      setItems(tasks);
-      //console.log(tasks)
-    };
-    lists();
-    return () => {};
-  }, [items]);
+      let tasks = await axios.get("http://localhost:9000/todo")
+      tasks = tasks.data
+      let newT = tasks.map((obj)=>({...obj, style:obj.done?'done':'notDone'}))
+      setItems(newT)
+    }
+    lists()
+    return () => {}
+  }, [items])
 
   async function handelSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
     await axios.post("http://localhost:9000/todo", {
       name: item,
-    });
-    setItem("");
+    })
+    setItem("")
   }
   function handelChange(e) {
-    setItem(e.target.value);
+    setItem(e.target.value)
   }
 
   async function handelDelete(id) {
-    await axios.delete("http://localhost:9000/todo/" + id);
+    await axios.delete("http://localhost:9000/todo/" + id)
   }
   async function handelEdit(id) {
-    setItemId(id);
+    setItemId(id)
+  }
+  async function handelClik(id, i){
+    let don = await axios.get("http://localhost:9000/todo/" + id)
+    don=don.data.done
+    don = !don
+    await axios.patch("http://localhost:9000/todo/" + id, {
+      done: don,
+    })
   }
   return (
     <div className="App">
@@ -43,18 +54,18 @@ function Home({ setItemId }) {
       {items.length > 0
         ? items.map((e, i) => {
             return (
-              <div key={e._id}>
-                <h2>{e.name}</h2>
+              <div id="task" key={e._id} onClick={()=>handelClik(e._id, i)}>
+                <h2 className={e.style}>{e.name}</h2>
                 <button onClick={() => handelEdit(e._id)}>
                   <Link to="/edit">edit</Link>
                 </button>
                 <button onClick={() => handelDelete(e._id)}>delete</button>
               </div>
-            );
+            )
           })
         : "No items"}
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
